6975993d61e0cf849f909ec10d84052f3c2d294a
jadedResearcher <gtg287y@gmail.com>
2019-06-16 13:12:02 -0400

    image staging is empty???

--- web/loras2.dart
+++ web/loras2.dart
@@ -1,6 +1,22 @@
 import 'dart:html';
 
+String caption;
+const String podUrl = "http://farragnarok.com/PodCasts/";
+
 void main()
 {
-    window.alert("Future JR: be sure to hack images image caches. You can find them by id.");
+    String initPW = "";
+    if(Uri.base.queryParameters['passPhrase'] != null) {
+        initPW = Uri.base.queryParameters['passPhrase'];
+    }
+
+    caption = initPW;
+    hack(caption);
+    print("Future JR: be sure to hack images image caches. You can find them by id.");
+    //todo: do NOT actually wire this into anything just yet, just start out with testing things. woudln't do to spoil the whole thing for the wastes
+}
+
+void hack(String hack) {
+
+
 }
